16612aa297699111a4088c11bc5f3af55309ebf8
Jummy007 <gbemisoyeadejumoke@gmail.com>
2024-09-12 16:14:55 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,34 @@
+function addTwoNumbers() {
+  console.log(2 + 3);
+}
+addTwoNumbers();
+
+function addAnotherSet() {
+  console.log(3 + 4);
+}
+addAnotherSet();
+
+function addTen() {
+  console.log(10 + 10);
+}
+addTen();
+
+// parameter and argument
+
+function sumNumbers(a, b) {
+  console.log(a + b);
+}
+sumNumbers(2, 3);
+sumNumbers(3, 4);
+
+// /arrow functiion
+const multiply = () => {
+  alert("HIIIII");
+};
+multiply();
+
+const addNumbers = (a, b) => {
+  console.log(a + b);
+};
+addNumbers(2, 4);
+addNumbers(3, 3);
---
+++ main.js
@@ -0,0 +1,32 @@
+// alert(2+2);
+// var response= prompt("What is your name");
+// js variables
+// let, const, var are keywords in declaring variables
+var x = 56;
+var frontend = "HTML";
+const names = 3;
+let isOnline = false;
+
+let person = "Bunmi";
+var conj = "is";
+console.log("This is frontend class");
+console.log(2 * 3);
+console.log(person + " " + conj + " eating");
+console.log(person + conj + "drinking");
+console.log(person + conj + "sweeping");
+// console.log(response);
+
+// function myFunction() {
+//   alert("I am a function");
+// }
+// myFunction();
+
+function addNumbers() {
+  return 3 + 4;
+}
+console.log(addNumbers());
+
+function subtract() {
+  console.log(4 - 2);
+}
+subtract();
